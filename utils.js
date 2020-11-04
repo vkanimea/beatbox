f2090705405c2a7f344b1eaac78257a373c85639
Vincent Kanimea <vincentk@spc.int>
2020-11-04 12:14:06 +1200

    Button pt2

--- utils.js
+++ utils.js
@@ -29,21 +29,21 @@ class Button {
        this.keyCode = keyCode;
        this.element = document.getElementById(keyCode);
        this.setButtonColorInHTML();
-
     }
 
     /**
      * Set the button color based on color specified
      */
     setButtonColorInHTML = () => {
-
-        
+        this.element.style.borderColor = this.color;
     }
 
     /**
      * Select function to set the background color and boxShadow
      */
     select = () => {
+        this.element.style.backgroundColor = this.color;
+        this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
     }
 
     /**
